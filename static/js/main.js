13d254e84aa589a318727cc4dd06dc6f9d7ce2d8
David Mensah <davidmensahedem@gmail.com>
2024-04-10 04:53:56 +0000

    Add short term rentals and traffic incidents layers

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 
-
     // Leaflet Js Implementations
     var map = L.map('map').setView([51.0447, -114.0719], 13);
 
@@ -17,24 +16,26 @@ $(document).ready(function () {
         .openPopup();
 
 
-    // Load CSV file and convert to GeoJSON
-    let schoolLocationsGeoJson = loadCSV('static/data/School_Locations.csv');
-
-    L.geoJSON(schoolLocationsGeoJson, {
-        pointToLayer: function(feature, latlng) {
-            var marker = L.marker(latlng, {
-                icon: L.icon({
-                    iconUrl: 'static/image/school.png',
-                    iconSize: [20, 20],
-                    iconAnchor: [12, 41],
-                    popupAnchor: [1, -34],
-                    shadowSize: [41, 41]
-                })
-            });
-            marker.bindPopup(JSON.stringify(feature.properties));
-            return marker;
-        }
+    var trafficIncidents = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'ericakuamoah06/clutb4152001a01qx1jeh5zm2', // Replace with your own Mapbox style ID
+        accessToken: 'pk.eyJ1IjoiZXJpY2FrdWFtb2FoMDYiLCJhIjoiY2x0Ym9xaGEyMXY1ZzJrcGR5aDZhdjJ3cyJ9.JAiPKmIviFoezy5HMfLgYA'
     }).addTo(map);
+
+    var shortTermRentals = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'ericakuamoah06/clutbwa9n001d01piby2zdq9n', // Replace with your own Mapbox style ID
+        accessToken: 'pk.eyJ1IjoiZXJpY2FrdWFtb2FoMDYiLCJhIjoiY2x0Ym9xaGEyMXY1ZzJrcGR5aDZhdjJ3cyJ9.JAiPKmIviFoezy5HMfLgYA'
+    }).addTo(map);
+    
+
+    var baseLayers = {
+        "Traffic Incidents": trafficIncidents,
+        "Short Term Rentals": shortTermRentals,
+    };
+
+    layercontrol = L.control.layers(baseLayers).addTo(map);
+
 
 
 
@@ -60,7 +61,7 @@ $(document).ready(function () {
     // Function to get user's location coordinates
     function getUserLocation() {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
+            navigator.geolocation.getCurrentPosition(function (position) {
                 var latitude = position.coords.latitude;
                 var longitude = position.coords.longitude;
                 var coordinates = "Latitude: " + latitude + ", Longitude: " + longitude;
@@ -77,59 +78,6 @@ $(document).ready(function () {
 
 
 
-
-
-
-
-
-    // Function to load CSV file and convert to GeoJSON
-    function loadCSV(csvFilePath) {
-        Papa.parse(csvFilePath, {
-            download: true,
-            header: true,
-            dynamicTyping: true,
-            complete: function(results) {
-                var data = results.data;
-                var features = [];
-                data.forEach(function(row) {
-                    var lat = row.latitude;
-                    var lon = row.longitude;
-                    if (lat && lon) {
-                        features.push({
-                            type: 'Feature',
-                            properties: row,
-                            geometry: {
-                                type: 'Point',
-                                coordinates: [lon, lat]
-                            }
-                        });
-                    }
-                });
-
-                var geojson = {
-                    type: 'FeatureCollection',
-                    features: features
-                };
-
-                L.geoJSON(geojson, {
-                    pointToLayer: function(feature, latlng) {
-                        var marker = L.marker(latlng, {
-                            icon: L.icon({
-                                iconUrl: 'static/image/school.png',
-                                iconSize: [20, 20],
-                                iconAnchor: [12, 41],
-                                popupAnchor: [1, -34],
-                                shadowSize: [41, 41]
-                            })
-                        });
-                        marker.bindPopup(JSON.stringify(feature.properties));
-                        return marker;
-                    }
-                }).addTo(map);
-
-            }
-        });
-    }
 
 });
 
